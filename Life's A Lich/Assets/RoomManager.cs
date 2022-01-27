dbf93f2fed93bbfe39ecbb3c89f34b67a29ba8dc
WazaXESoftware <98400999+WazaXESoftware@users.noreply.github.com>
2022-01-27 17:07:43 +0100

    Stoffe Cam Script

---
+++ Life's A Lich/Assets/RoomManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+    public GameObject virtualCam;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger)
+        {
+            virtualCam.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger)
+        {
+            virtualCam.SetActive(false);
+        }
+    }
+
+}
