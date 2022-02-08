825ab269b3b7f2073cd91827537f4b3394394dfe
alex00a <alexander@familjenandersson.se>
2022-02-08 10:20:12 +0100

    Dialog tillägg.
    push för att kunna göra en pull

--- Life's A Lich/Assets/Scenes/Alex Test Saker/Test Scripts/DialogueManager.cs
+++ Life's A Lich/Assets/Scenes/Alex Test Saker/Test Scripts/DialogueManager.cs
@@ -9,4 +9,9 @@ public class DialogueManager : MonoBehaviour
     {
         sentences = new Queue<string>();
     }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+
+    }
 }
--- Life's A Lich/Assets/Scenes/Alex Test Saker/Test Scripts/DialogueTrigger.cs
+++ Life's A Lich/Assets/Scenes/Alex Test Saker/Test Scripts/DialogueTrigger.cs
@@ -5,4 +5,9 @@ using UnityEngine;
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
 }
