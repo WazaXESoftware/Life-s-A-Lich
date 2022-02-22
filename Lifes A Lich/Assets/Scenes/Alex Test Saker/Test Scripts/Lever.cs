4b9de88ea94e1ac1b9ea2ae64cf720119b8f252c
alex00a <alexander@familjenandersson.se>
2022-02-22 16:48:12 +0100

    Lite olika saker

---
+++ Lifes A Lich/Assets/Scenes/Alex Test Saker/Test Scripts/Lever.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Lever : InteractableObject
+{
+    public GameObject door;
+    public float speedUp = 3f;
+    public float speedDown = 4f;
+    public bool buttonPushed;
+    public float minHeight;
+    public float maxHeight;
+    public bool leverPulled;
+    void Start()
+    {
+        
+    }
+
+    public override void Interact()
+    {
+        
+    }
+    void Update()
+    {
+        if(leverPulled && door.transform.position.y < maxHeight)
+        {
+            moveUp();
+        }
+        else if (leverPulled == false && door.transform.position.y > minHeight)
+        {
+            moveDown();
+        }
+    }
+
+    private void moveUp()
+    {
+        door.transform.Translate(Vector3.up * speedUp * Time.deltaTime);
+    }
+
+    private void moveDown()
+    {
+        door.transform.Translate(Vector3.down * speedDown * Time.deltaTime);
+    }
+}
