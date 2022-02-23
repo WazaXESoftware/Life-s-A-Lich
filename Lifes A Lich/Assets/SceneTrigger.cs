bdff86eefadcdca5fed7963992c57cfd1c38ab8d
WazaXESoftware <98400999+WazaXESoftware@users.noreply.github.com>
2022-02-23 16:57:34 +0100

    Working on scene transition

---
+++ Lifes A Lich/Assets/SceneTrigger.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTrigger : MonoBehaviour
+{
+    public Animator animator;
+    private int levelToLoad;
+
+ // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Submit"))
+        {
+            FadeToLevel(+1);
+        }
+    }
+
+    public void FadeToLevel(int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene("MainTwo");
+
+    }
+}
