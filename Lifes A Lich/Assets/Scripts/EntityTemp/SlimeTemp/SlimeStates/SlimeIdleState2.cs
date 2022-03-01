378a1adac5311168b04eaabafebf864625d7a102
FelixLWilson <b20felwi@student.his.se>
2022-03-01 15:27:16 +0100

    Bugfix?

--- Lifes A Lich/Assets/Scripts/EntityTemp/SlimeTemp/SlimeStates/SlimeIdleState2.cs
+++ Lifes A Lich/Assets/Scripts/EntityTemp/SlimeTemp/SlimeStates/SlimeIdleState2.cs
@@ -25,56 +25,6 @@ public class SlimeIdleState2 : SlimeState2
     public override void PlayerUpdate()
     {
         Controls();
-        if (!Input.GetKey(KeyCode.Space))
-        {
-            /**
-            if (entity.chargeTimer > 0)
-            {
-                entity.animator.SetTrigger("OnRelease");
-                if (entity.chargeTimer > entity.leastChargeForSkip)
-                {
-                    float trueTime = Mathf.Clamp(entity.chargeTimer, 0f, entity.skipMaxChargeTime);
-                    Vector3 forward = new Vector3(entity.cameraMain.transform.forward.x, 0, entity.cameraMain.transform.forward.z).normalized;
-                    Vector3 right = Vector3.Cross(Vector3.up, forward).normalized;
-
-                    float horizontal = Input.GetAxisRaw("Horizontal");
-                    float vertical = Input.GetAxisRaw("Vertical");
-                    Vector3 dir = (forward * vertical + right * horizontal).normalized;
-                    if (dir.magnitude != 0) entity.transform.rotation = Quaternion.LookRotation(dir, Vector3.up);
-
-                    entity.body.velocity = (trueTime / entity.skipMaxChargeTime) * dir * entity.skipForceX + new Vector3(0, (trueTime / entity.skipMaxChargeTime) * entity.skipForceY, 0);
-                    ExitState(entity.jumpState);
-                }
-            }
-            **/
-            //else
-            //{
-                entity.Movement();
-
-                /**
-                Vector3 forward = new Vector3(entity.cameraMain.transform.forward.x, 0, entity.cameraMain.transform.forward.z).normalized;
-                Vector3 right = Vector3.Cross(Vector3.up, forward).normalized;
-                float horizontal = Input.GetAxisRaw("Horizontal");
-                float vertical = Input.GetAxisRaw("Vertical");
-                Vector3 dir = (forward * vertical + right * horizontal).normalized;
-                if (dir.magnitude != 0) entity.transform.rotation = Quaternion.LookRotation(dir, Vector3.up);
-                entity.body.velocity = dir * entity.moveSpeed + new Vector3(0, entity.body.velocity.y, 0);
-                entity.body.AddForce(dir * entity.moveSpeed * Time.deltaTime, ForceMode.VelocityChange);
-                **/
-           // }
-
-            //entity.chargeTimer = 0f;
-        }
-        else
-        {
-            ExitState(entity.chargeState);
-
-            /**
-            if (entity.chargeTimer == 0) entity.animator.SetTrigger("OnChargeUp");
-            entity.body.AddForce(new Vector3(-entity.body.velocity.x * 0.97f * Time.deltaTime, 0, -entity.body.velocity.z * 0.97f * Time.deltaTime), ForceMode.VelocityChange);
-            entity.chargeTimer += Time.deltaTime;
-            **/
-        }
 
         if (!entity.IsGrounded()) ExitState(entity.jumpState);
 
@@ -93,6 +43,7 @@ public class SlimeIdleState2 : SlimeState2
     public override void Controls()
     {
         if (entity.frozen) return;
+
         if (!Input.GetButton("Jump"))
         {
             entity.Movement();
@@ -116,11 +67,5 @@ public class SlimeIdleState2 : SlimeState2
             ExitState(entity.confusedState);
             entity.Exit();
         }
-    }
-
-    private bool IsGrounded()
-    {
-        //return entity.IsGrounded;
-        return Physics.CheckSphere(entityCollider.bounds.center - new Vector3(0, 0.25f, 0), (entityCollider.radius * entity.transform.localScale.y) - 0.1f, entity.layerMask, QueryTriggerInteraction.Ignore);
     }
 }
