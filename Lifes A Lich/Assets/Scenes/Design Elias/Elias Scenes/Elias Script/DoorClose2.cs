97507b4032a92c3582026a861cb70ac527cd1327
Boobins <elias.albrektson@live.se>
2022-03-17 11:25:24 +0100

    Dörrstängning i scen 2 första gungbrädan

---
+++ Lifes A Lich/Assets/Scenes/Design Elias/Elias Scenes/Elias Script/DoorClose2.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class DoorClose2 : MonoBehaviour
+{
+  public GameObject door;
+  private float minHeight;
+    // Start is called before the first frame update
+[SerializeField] Lever lever;
+void Start()
+{
+    minHeight = door.transform.position.y;
+}
+
+
+    private void OnTriggerEnter(Collider other){
+      if (other.CompareTag("Player") && door.transform.position.y > minHeight){
+        lever.leverPulled = false;
+
+      }
+    }
+
+}
